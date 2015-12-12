376033de42307dbd3dc205bf4f93000b555feb99
khaitq <khaitq@trans-macbook-pro.home>
2015-12-12 13:40:48 +0100

    Signed-off-by: khaitq <khaitq@trans-macbook-pro.home>

---
+++ DesignPattern/src/adapter/CarController.java
@@ -0,0 +1,27 @@
+package adapter;
+
+import java.util.List;
+
+import models.car.Car;
+
+public class CarController {
+	
+	public  void importAllCar(List<Car> listCar) {
+		saveToDb(listCar);
+		
+		displayNewItems(listCar);
+		
+	}
+	
+	private void saveToDb(List<Car> listCar){
+		
+	}
+	
+	private void displayNewItems(List<Car> listCar){
+		for(Car car : listCar){
+			System.out.println(car.toString());
+		}
+	}
+	
+
+}
