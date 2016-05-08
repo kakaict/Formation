c98f338ef7e42b7fb5916b50fe6216d76aba17e6
khaitq <khaitq@trans-macbook-pro.home>
2016-05-08 16:39:52 +0200

    vcxv

--- DesignPattern/src/adapter/CarListAdapter.java
+++ DesignPattern/src/adapter/CarListAdapter.java
@@ -22,13 +22,13 @@ public class CarListAdapter {
 		
 		List<Car> listCarImport = new ArrayList<Car>();
 		
-		Map<Integer, String> listCarName = carManager.getAllCar();
-		Iterator<Entry<Integer, String>> it  =  listCarName.entrySet().iterator();
+		Map<Integer, String> listCarName 		= carManager.getAllCar();
+		Iterator<Entry<Integer, String>> it  	=  listCarName.entrySet().iterator();
 		while(it.hasNext()){
-			Entry<Integer, String>	e 			= 	it.next();
-			Integer					carTypeId 	= 	e.getKey();
-			String					carName 	=	e.getValue();
-			Car car = CarFactory.getCar(CarType.getCarType(carTypeId));
+			Entry<Integer, String>	e 			= it.next();
+			Integer					carTypeId 	= e.getKey();
+			String					carName 	= e.getValue();
+			Car car 							= CarFactory.getCar(CarType.getCarType(carTypeId));
 			car.setName(carName);
 			listCarImport.add(car);
 		}
